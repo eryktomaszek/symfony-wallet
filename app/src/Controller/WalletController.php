222ea6c7d185ebcda86b9030a104649cf7a0e2f3
eryktomaszek <ervk.tomvszek@gmail.com>
2024-06-05 10:26:29 +0200

    Added date formatting extension

--- app/src/Controller/WalletController.php
+++ app/src/Controller/WalletController.php
@@ -8,6 +8,7 @@ use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
