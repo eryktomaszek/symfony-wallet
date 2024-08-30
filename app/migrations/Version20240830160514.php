be5ca2eeee35136d9beb7bbc9f76c141c9db2ced
eryktomaszek <ervk.tomvszek@gmail.com>
2024-08-30 18:06:48 +0200

    New device test commit

--- app/migrations/Version20240830160514.php
+++ app/migrations/Version20240830160514.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240731152216 extends AbstractMigration
+final class Version20240830160514 extends AbstractMigration
 {
     public function getDescription(): string
     {
